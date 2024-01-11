c164534014449035d34c74f207bbc6f26f541b86
İrem Akın Tunga <iremakin00@gmail.com>
2024-01-11 15:36:38 +0300

    adds distance method with two int parameters

--- src/Point.java
+++ src/Point.java
@@ -30,4 +30,8 @@ public class Point {
     public double distance(Point p) {
         return Math.sqrt(Math.pow(p.x - this.x, 2) + Math.pow(p.y - this.y, 2));
     }
+
+    public double distance(int a, int b) {
+        return Math.sqrt(Math.pow(a - this.x, 2) + Math.pow(b - this.y, 2));
+    }
 }
